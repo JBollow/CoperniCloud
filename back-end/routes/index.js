7b8a5ee599488f43b2459f386006cc2658a85002
annaformaniuk <a_form@uni-muenster.de>
2017-12-03 19:18:07 +0100

    Bugfix

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -2,18 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 //filesearch
-const testFolder = 'Y://OneDrive/Dokumente/Uni/Uni Münster/WS17/Geosoft 2/Projekt/Testdaten/opt/sentinel2';
+const testFolder = 'F://Dokumente/Uni/WS_2017/Geosoft2/Testdaten/opt/sentinel2';
 const fs = require('fs');
 var fileNames = [];
 
 fs.readdir(testFolder, (err, files) => {
-    files.forEach(file => {
-        fileNames.push(file);
-        console.log(file);
-    });
+    if (files) {
+        files.forEach(file => {
+            fileNames.push(file);
+            console.log(file);
+        });
+    }
 })
 
-router.get('/search', function() {
+router.get('/search', function () {
     console.log("Hallo");
 })
 
