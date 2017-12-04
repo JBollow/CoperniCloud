31b4283f0ee8f125e42fc8138c0bae88d1e52483
Jan-Patrick Bollow <jan_patrickbollow@hotmail.com>
2017-12-04 04:38:37 +0100

    app.js überarbeitet

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -2,20 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 //filesearch
-const testFolder = 'F://Dokumente/Uni/WS_2017/Geosoft2/Testdaten/opt/sentinel2';
+const testFolder = '../../Testdaten/opt/sentinel2';
 const fs = require('fs');
 var fileNames = [];
 
 fs.readdir(testFolder, (err, files) => {
-    if (files) {
-        files.forEach(file => {
-            fileNames.push(file);
-            console.log(file);
-        });
-    }
+    files.forEach(file => {
+        fileNames.push(file);
+        console.log(file);
+    });
 })
 
-router.get('/search', function () {
+router.get('/search', function() {
     console.log("Hallo");
 })
 
