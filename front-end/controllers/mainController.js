a0eedb7108aa4a3eb34db3383f0a0ffc45097da9
JBollow <Jan_PatrickBollow@hotmail.com>
2017-12-21 11:21:31 +0100

    docker tiles

--- front-end/controllers/mainController.js
+++ front-end/controllers/mainController.js
@@ -216,7 +216,9 @@ coperniCloud.controller('mainController', ['$scope', '$timeout', 'leafletData', 
         if ($scope.tilesLayer) {
             $scope.baseMap.removeLayer($scope.tilesLayer);
         }
-        $scope.tilesLayer = L.tileLayer('http://127.0.0.1:8887/' + folderName + '.SAFE/TCI/{z}/{x}/{y}.png', {
+        // Jetzt nur TCI, restlichen Bänder vielleicht über eine Auwahl?
+        // http://gis-bigdata:12015/tiles/ klappt nur im uni vpn und wenn der server läuft
+        $scope.tilesLayer = L.tileLayer('http://gis-bigdata:12015/tiles/' + folderName + '.SAFE/TCI/{z}/{x}/{y}.png', {
             attribution: 'Tiles',
             tms: true,
             minZoom: 3,
