464f11ebaa1ecb7da5e0d63a223893d2f6c333cd
Jan-Patrick Bollow <jan_patrickbollow@hotmail.com>
2018-02-13 10:16:34 +0100

    start.sh dass hoffentlich alles im hintergrund weiter läuft

--- start.sh
+++ start.sh
@@ -4,13 +4,13 @@ cd /opt/
 sudo python3 -m http.server 443 >/dev/null 2>&1 & #
 cd /home/t_kueh06/copernicloud/Geosoftware-2/back-end
 echo "Starting back-end"
-npm start &
+nodemon app >/dev/null 2>&1 & #
 cd /home/t_kueh06/copernicloud/Geosoftware-2/front-end
 echo "Starting front-end"
-lite-server &
+lite-server >/dev/null 2>&1 & #
 cd /home/t_kueh06/copernicloud/Geosoftware-2/py_imgProcessing
 echo "Starting py_imgProcessing"
-sudo python3 bottle_server.py &
+sudo python3 bottle_server.py >/dev/null 2>&1 & #
 cd /home/t_kueh06/
 echo "Starting mongodDB"
 sudo mongod --dbpath=/home/t_kueh06/mongoDB_data/ >/dev/null 2>&1 & #
